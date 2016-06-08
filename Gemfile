68ea2974fa19c2374dc95061984c542ae07bfba4
Andrew Kreps <andrew.kreps@gmail.com>
2016-06-08 11:02:03 -0700

    Created basic service for CSE ops.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'onewheel-google', '~> 0'
---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra/base'
+require 'onewheel-google'
+require 'sinatra/config_file'
+
+class App < Sinatra::Base
+  register Sinatra::ConfigFile
+  config_file 'config.yml'
+
+  get '/google' do
+    # content_type 'application/json'
+    result = OnewheelGoogle::search('google', settings.cse_id, settings.api_key, 'high')
+    "#{result['items'][0]['link']} #{result['items'][0]['title']}: #{result['items'][0]['snippet']}"
+  end
+end
