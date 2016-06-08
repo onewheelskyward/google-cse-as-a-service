ea2af162ea8f41eafde48a467d2c734eac641c62
Andrew Kreps <andrew.kreps@gmail.com>
2016-06-08 13:19:41 -0700

    Putting the application on the root path.

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   register Sinatra::ConfigFile
   config_file 'config.yml'
 
-  get '/google' do
+  get '/' do
     # content_type 'application/json'
     result = OnewheelGoogle::search('google', settings.cse_id, settings.api_key, 'high')
     "#{result['items'][0]['link']} #{result['items'][0]['title']}: #{result['items'][0]['snippet']}"
