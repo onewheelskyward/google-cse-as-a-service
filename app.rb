e9a1e87e56e3507fd1544b45cf1f30593d028bb8
Andrew Kreps <andrew.kreps@gmail.com>
2016-06-08 21:48:14 -0700

    Slack output format.

--- app.rb
+++ app.rb
@@ -6,9 +6,18 @@ class App < Sinatra::Base
   register Sinatra::ConfigFile
   config_file 'config.yml'
 
-  get '/' do
-    # content_type 'application/json'
+  get '/google' do
+    content_type 'application/json'
+
+    puts params.inspect
+
     result = OnewheelGoogle::search('google', settings.cse_id, settings.api_key, 'high')
-    "#{result['items'][0]['link']} #{result['items'][0]['title']}: #{result['items'][0]['snippet']}"
+    {
+      response_type: 'in_channel',
+      text: result['items'][0]['link'],
+      attachments: [{
+        text: "#{result['items'][0]['title']}: #{result['items'][0]['snippet']}"
+      }]
+    }.to_json
   end
 end
